9719d5dceab32aa22435947751f63ce5a2f0d543
Nicolas Azrak <nazrak@medallia.com>
2020-11-06 19:45:45 -0300

    Use dataset instead of eagerly loading data

---
+++ net/dataset.py
@@ -0,0 +1,35 @@
+from glob import glob
+from math import floor
+import tensorflow as tf
+from random import shuffle
+
+
+def load_audio(file_path):
+    binary = tf.io.read_file(file_path)
+    decoded, _ = tf.audio.decode_wav(binary, desired_channels=1, desired_samples=44100)
+    return decoded
+
+
+def to_label(file_path):
+    if "background" in file_path:
+        return tf.constant(0)
+    return tf.constant(1)
+
+
+def make_datasets():
+    filenames = glob("training-data/*/*.wav")
+    shuffle(filenames)
+
+    split_idx = floor(len(filenames) * 0.8)
+
+    train_filenames = filenames[:split_idx]
+    train_audios = tf.data.Dataset.from_tensor_slices(train_filenames).map(load_audio)
+    train_labels = tf.data.Dataset.from_tensor_slices(list(map(to_label, train_filenames)))
+    train_dataset = tf.data.Dataset.zip((train_audios, train_labels)).batch(32)
+
+    test_filenames = filenames[split_idx:]
+    test_audios = tf.data.Dataset.from_tensor_slices(test_filenames).map(load_audio)
+    test_labels = tf.data.Dataset.from_tensor_slices(list(map(to_label, test_filenames)))
+    test_dataset = tf.data.Dataset.zip((test_audios, test_labels)).batch(32)
+
+    return train_dataset, test_dataset
--- net/main.py
+++ net/main.py
@@ -1,8 +1,7 @@
 import tensorflow as tf
 import numpy as np
-from glob import glob
-from math import floor
-from random import shuffle
+import os.path
+from dataset import make_datasets
 from keras import *
 from keras.layers import *
 from log_mel_spectogram import LogMelSpectrogram
@@ -10,64 +9,12 @@ from keras.callbacks import ModelCheckpoint
 from kapre import STFT, Magnitude, MagnitudeToDecibel
 
 
-def make_datasets():
-    all_files = []
-    for file_path in glob("training-data/background/*.wav"):
-        all_files.append((file_path, 0))
-    for file_path in glob("training-data/positive-noisy/*.wav"):
-        all_files.append((file_path, 1))
-    for file_path in glob("training-data/positive-clean/*.wav"):
-        all_files.append((file_path, 1))
-    shuffle(all_files)
-    idx = floor(len(all_files) * 0.8)
-    return all_files[:idx], all_files[idx:]
-
-
-def generator_for(dataset, batch_size=64):
-    current_batch_x = []
-    current_batch_y = []
-
-    for audio_path, label in dataset:
-        audio = tf.io.read_file(audio_path)
-        decoded, sr = tf.audio.decode_wav(
-            audio, desired_channels=1, desired_samples=44100)
-
-        current_batch_x.append(decoded.numpy())
-        current_batch_y.append(tf.constant([label]))
-
-        if len(current_batch_x) >= batch_size:
-            yield (tf.constant(current_batch_x), tf.constant(current_batch_y))
-            current_batch_x.clear()
-            current_batch_y.clear()
-
-    if len(current_batch_x) > 0:
-        yield (tf.constant(current_batch_x), tf.constant(current_batch_y))
-
-
-def make_pairs(dataset, batch_size=64):
-    X = []
-    Y = []
-
-    for audio_path, label in dataset:
-        audio = tf.io.read_file(audio_path)
-        decoded, sample_rate = tf.audio.decode_wav(
-            audio, desired_channels=1, desired_samples=44100)
-        X.append(decoded.numpy())  # This sucks I know
-        Y.append(np.array([label]))
-
-    X = tf.constant(X)
-    Y = tf.constant(Y)
-    return X, Y
-
-
 def build_rnn_model(sample_rate, duration, fft_size, hop_size, n_mels):
     n_samples = sample_rate * duration
 
     X_input = tf.keras.Input(shape=(n_samples,))
 
-    X = LogMelSpectrogram(sample_rate, fft_size, hop_size,
-                          n_mels, expand_channels=False)(X_input)
-
+    X = LogMelSpectrogram(sample_rate, fft_size, hop_size, n_mels, expand_channels=False)(X_input)
     X = BatchNormalization()(X)
 
     X = Conv1D(filters=128, kernel_size=3, strides=1)(X)
@@ -99,15 +46,11 @@ model = build_rnn_model(
     n_mels=512
 )
 
-checkpoint = ModelCheckpoint("model.hdf5", monitor='loss',
-                             verbose=1, save_weights_only=True, save_best_only=True, mode='auto')
+checkpoint = ModelCheckpoint("model.hdf5", monitor='loss', verbose=1, save_weights_only=True, save_best_only=True, mode='auto')
 
 # model.load_weights("model.hdf5")
 
-
-train, test = make_datasets()
-x_train, y_train = make_pairs(train)
-x_test, y_test = make_pairs(test)
+train_dataset, test_dataset = make_datasets()
 
 model.summary()
 model.compile(optimizer="adam", loss="binary_crossentropy", metrics=[
@@ -115,8 +58,7 @@ model.compile(optimizer="adam", loss="binary_crossentropy", metrics=[
     tf.keras.metrics.BinaryAccuracy(name='90_threshold', threshold=0.9),
 ])
 
-model.fit(x=x_train, y=y_train, validation_data=(
-    x_test, y_test), epochs=50, callbacks=[checkpoint])
+model.fit(x=train_dataset, validation_data=test_dataset, epochs=50, callbacks=[checkpoint])
 
-loss = model.evaluate(x_test,  y_test, verbose=2)
+#loss = model.evaluate(x_test,  y_test, verbose=2)
 # print("Restored model, accuracy: {:5.2f}%".format(100*acc))
