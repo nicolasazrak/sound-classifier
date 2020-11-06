74d6fd1a2c3485bcc26e8616aae12be89a3641c3
Nicolas Azrak <nazrak@medallia.com>
2020-11-06 18:29:43 -0300

    Model and path fixes

--- recorder/cropper.py
+++ recorder/cropper.py
@@ -1,4 +1,3 @@
-import pyaudio
 import wave
 import threading
 import sys
@@ -25,7 +24,7 @@ def generate_thumbs():
         if not os.path.isfile(os.path.join("training-data", "raw", recording + ".png")):
             print("Generating training-data/raw/{recording}.png")
             os.system(
-                f'ffmpeg -i "training-data/raw/{recording}"  -filter_complex "[0:a]aformat=channel_layouts=mono,  compand=gain=10,  showwavespic=s=600x120:colors=#9cf42f[fg];  color=s=600x120:color=#44582c,  drawgrid=width=iw/10:height=ih/5:color=#9cf42f@0.1[bg];  [bg][fg]overlay=format=auto,drawbox=x=(iw-w)/2:y=(ih-h)/2:w=iw:h=1:color=#9cf42f" -frames:v 1 "recordings/raw/{recording}.png"')
+                f'ffmpeg -i "training-data/raw/{recording}"  -filter_complex "[0:a]aformat=channel_layouts=mono,  compand=gain=10,  showwavespic=s=600x120:colors=#9cf42f[fg];  color=s=600x120:color=#44582c,  drawgrid=width=iw/10:height=ih/5:color=#9cf42f@0.1[bg];  [bg][fg]overlay=format=auto,drawbox=x=(iw-w)/2:y=(ih-h)/2:w=iw:h=1:color=#9cf42f" -frames:v 1 "training-data/raw/{recording}.png"')
 
 
 # def generate_background():
